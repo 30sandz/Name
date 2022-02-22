c366edb9c787be08643fbc43ff54cec18229aa4c
gagaan-info <100069895+gagaan-info@users.noreply.github.com>
2022-02-22 22:35:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Mar 3, 2022 00:00:00:").getTime();
+var countDownDate = new Date("Mar 5, 2022 00:00:00:").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
     var distance = countDownDate - now;
