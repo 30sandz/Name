53254554cb1a3f13d9aa3dbd5baefa815f14f888
gagaan-info <100069895+gagaan-info@users.noreply.github.com>
2022-03-09 11:20:27 +0530

    Change

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var x = setInterval(function(){
         document.getElementById("hours").innerHTML = "00";
         document.getElementById("minutes").innerHTML = "00";
         document.getElementById("seconds").innerHTML = "00";
-        alert("Happy Birthday Monisha");
-        location.href = '/Monisha/home.html'
+        alert("Happy Birthday");
+        location.href = '/Name/home.html'
     }
 },1000);
